7777a8eb2a8a7602afac2e02e9581c1ba195d35e
Michael James <michael@basicby.design>
2022-03-10 08:49:48 +0100

    removed freemius integration

---
+++ tags/1.1/lib/chetmac/Airpress.php
@@ -0,0 +1,454 @@
+<?php
+
+class Airpress {
+
+	private $virtualPosts;
+	private $virtualFields;
+	public $debug_output;
+	private $currentRecord;
+	public $debug_stats;
+
+	private $deferred_queries;
+
+	function init(){
+
+		airpress_debug(0,"\n\nAIRPRESS LOADED");
+
+		$this->deferredQueries = array();
+
+		$this->virtualFields = new AirpressVirtualFields();
+		$this->virtualFields->init();
+
+		$this->virtualPosts = new AirpressVirtualPosts();
+		$this->virtualPosts->init();
+
+		// Populate related field with related records from another table
+		add_shortcode( 'apr_populate', array($this,'shortcode_populate') );
+
+		// Display contents of field
+		add_shortcode( 'apr', array($this,'shortcode_display') );
+
+		// Include PHP file
+		add_shortcode( 'apr_include', array($this, 'shortcode_include'));
+
+		// Loop through collection or array
+		add_shortcode( 'apr_loop', array($this,'shortcode_loop') );
+		for($i=0;$i<=10;$i++){
+			add_shortcode( 'apr_loop'.$i, array($this,'shortcode_loop') );
+		}
+
+		add_action( 'wp_footer', array($this,'renderDebugOutput') );
+		add_action( 'admin_bar_menu', array($this,'renderDebugToggle'), 999 );
+		add_action( 'shutdown', array($this,'stash_and_trigger_deferred_queries'));
+	}
+
+
+/* CONFIGURATION FUNCTIONS */
+
+	
+	public function shortcode_loop($atts,$content=null,$tag){
+		global $post,$airpress;		
+
+	    $a = shortcode_atts( array(
+	        'field'				=> null,
+	    ), $atts );
+
+
+	    if ($a["field"] === null){
+	    	$records_to_loop = (array)$post->AirpressCollection;
+	    } else if (isset($this->currentRecord)){
+	    	// This is a nested loop!
+	    	$keys = explode("|",$a["field"]);
+	    	$field = array_shift($keys);
+	    	$records_to_loop = $this->currentRecord[$field]->getFieldValues($keys);
+			$previousRecord = $this->currentRecord;
+	    } else {
+			$keys = explode("|", $a["field"]);
+		    $records_to_loop = $post->AirpressCollection->getFieldValues($keys);
+	    }
+
+	    preg_match_all("/{{([^}]*)}}/", $content, $matches);
+
+	    $replacementFields = array_unique($matches[1]);
+
+	    $output = "";
+	    foreach($records_to_loop as $record){
+
+	    	$this->currentRecord = $record;
+
+	    	// Reset the template for each record
+	    	// By doing the shortcodes BEFORE any processing, we're ensuring that
+	    	// any nested loops ... or innermost loops are processed prior to outter most loops.
+	    	// if we don't do this all {{variables}} will be replaced by outter most loops.
+	    	airpress_debug(0,$content);
+	    	$template = do_shortcode($content);
+
+	    	foreach($replacementFields as $replacementField){
+
+	    		$keys = explode("|", $replacementField);
+	    		$field = array_shift($keys);
+				$replacementValue = "";
+				
+				if ( strtolower($field) == "record_id" ){
+					if ( is_airpress_record($record)){
+						$replacementValue = $record->record_id();
+					} else {
+						airpress_debug(0,"Attempting to populate field $field on a non-populated record",$keys);
+					}
+				} else if ( ! is_airpress_empty( $record[$field] ) ){ 
+					// this means it IS an AirpressCollection with records
+
+	    			if (empty($keys)){
+	    				// this shouldn't really happen because we can't output a collection
+	    				// we should be looking INSIDE the collection, but can't since keys is empty
+	    			} else {
+	    				$replacementValue = implode(", ", $record[$field]->getFieldValues($keys) );
+	    			}
+
+	    		// This field is an array
+	    		} else if (is_array($record[$field]) ){
+
+	    			if (empty($keys)){
+	    				$replacementValue = implode(", ",$record[$field]);
+	    			} else {
+	    				$array = $record[$field];
+	    				while (!empty($keys)){
+	    					$key = array_shift($keys);
+	    					$array = $array[$key];
+	    				}
+	    				if (is_array($array)){
+	    					$replacementValue = implode(", ",$array);
+	    				} else {
+	    					$replacementValue = $array;
+	    				}
+	    			}
+
+	    		} else if (isset($record[$field])){
+
+	    			$replacementValue = $record[$field];
+
+	    		} else {
+
+	    			$replacementValue = "";
+
+	    		}
+
+    			$template = str_replace("{{".$replacementField."}}", $replacementValue, $template);
+
+	    	}
+
+	    	$output .= $template;
+	    	unset($this->currentRecord);
+	    }
+
+	    if (isset($previousRecord)){
+	    	$this->currentRecord = $previousRecord;
+	    }
+
+	    return $output;
+	}
+
+	public function shortcode_populate($atts, $content=null, $tag){
+		global $post,$airpress;
+
+		// Check for non-value "flag" attributes. Set to true
+		$flags = array("single");
+		foreach($atts as $k => $v){
+			foreach($flags as $flag){
+				if (strtolower($v) == $flag ){
+					$atts[$flag] = true;
+				}
+			}
+		}
+
+	    $a = shortcode_atts( array(
+	        'field'				=> null,
+			'relatedto'			=> null,
+			'filterbyformula'	=> null,
+			'view'				=> null,
+			'sort'				=> null,
+			'maxrecords'		=> null,
+	    ), $atts );
+
+	    if (isset($post->AirpressCollection)){
+	    	$collection = $post->AirpressCollection;
+	    } else {
+	    	airpress_debug(0,"NO AIRPRESS COLLECTION with which to populate ".$a["field"]);
+	    	return null;
+	    }
+
+	    $keys = explode("|", $a["field"]);
+
+		// Gather IDs
+		$record_ids = $collection->getFieldValues($keys);
+
+		$query = new AirpressQuery();
+		$query->setConfig($collection->query->getConfig());
+	    $query->table($a["relatedto"]);
+	    $query->filterByRelated($record_ids);
+
+	    if (isset($a["filterbyformula"]))
+	    	$query->filterByFormula($a["filterbyformula"]);
+
+	    if (isset($a["view"]))
+	    	$query->view($a["view"]);
+
+	    if (isset($a["sort"]))
+	    	$query->view($a["sort"]);
+
+	    if (isset($a["maxrecords"]))
+	    	$query->maxRecords($a["maxrecords"]);
+
+		$subCollection = new AirpressCollection($query);
+		$collection->setFieldValues($keys,$subCollection,$query);
+
+	}
+
+	public function shortcode_include($atts, $content=null, $tag){
+		global $post,$model;
+
+	    $a = shortcode_atts( array(
+	        'path' => null,
+	        'value' => null,
+			'title' => null
+	    ), $atts );
+
+	    $a["path"] = ltrim($a["path"], '/');
+
+	    $filepath = apply_filters("airpress_include_path_pre",$a["path"]);
+	    $filepath = get_stylesheet_directory()."/".$a["path"].".php";
+	    $filepath = apply_filters("airpress_include_path",$filepath);
+
+	    if (is_file($filepath)){
+	    	ob_start();
+	    	include($filepath);
+	    	$html = ob_get_clean();
+	    	return do_shortcode($html);
+	    } else {
+	    	return basename($filepath)." not found.";
+	    }
+	}
+
+	public function shortcode_display($atts, $content=null, $tag){
+		global $post;
+
+		// Check for non-value "flag" attributes. Set to true
+		$flags = array("single");
+		foreach($atts as $k => $v){
+			foreach($flags as $flag){
+				if (strtolower($v) == $flag ){
+					$atts[$flag] = true;
+				}
+			}
+		}
+
+	    $a = shortcode_atts( array(
+	        'field'				=> null,
+			'relatedto'			=> null,
+			'recordtemplate'	=> null,
+			'relatedtemplate'	=> null,
+			'single'			=> null,
+			'rollup'			=> null,
+			'default'			=> null,
+			'format'			=> null,
+			'glue'				=> null,
+	    ), $atts );
+
+	    $field_name = $a["field"];
+
+	    $single = (!isset($a["single"]) || strtolower($a["single"]) == "false")? false : true;
+
+		$recordTemplate = (isset($a["recordtemplate"]))? $a["recordtemplate"] : "%s\n";
+		$relatedTemplate = (isset($a["relatedtemplate"]))? $a["relatedtemplate"] : "%s\n";
+
+
+   		$keys = explode("|", $a["field"]);
+
+		$collection = $post->AirpressCollection;
+
+		$output = "";
+
+		if ( ! is_airpress_empty($collection) ){
+
+			$values = $collection->getFieldValues($keys);
+
+			if (isset($a["format"])){
+
+				// a typical filter would be: date|Y-m-d
+				// resulting in a hookable filter of: airfield_filter_date
+
+				$f = explode("|", $a["format"]);
+				foreach($values as &$value):
+					if ( has_filter("airpress_shortcode_filter_".$f[0])){
+						$value = apply_filters("airpress_shortcode_filter_".$f[0],$value,$f[1]);
+					} else {
+						switch ($f[0]){
+							case "date":
+								$value = date($f[1],strtotime($value));
+							break;
+						}
+					}
+
+				endforeach;
+				unset($value);
+			}
+
+			if (isset($a["glue"])){
+				$output = implode($a["glue"], $values);
+			} else {
+				$output = implode(", ", $values);
+			}
+
+		}
+
+		// $output = "";
+		// foreach($collection as $record){
+		// 	foreach($record[$field_name] as $value){
+		// 		if (isset($keys)){
+		// 			$output .= sprintf($relatedTemplate,$this->array_traverse($value,$keys));
+		// 		} else {
+		// 			$output .= sprintf($relatedTemplate,$value);
+		// 		}
+		// 	}
+
+		// 	if ($single)
+		// 		break; // only do first record
+
+		// }
+
+		return apply_filters( 'airpress_shortcode_filter', $output, $atts, $content, $tag );
+
+	}
+
+	####################################
+	## DEFERRED
+	####################################
+
+	function stash_and_trigger_deferred_queries(){
+
+		if (!empty($this->deferredQueries)){
+
+			$stash_key = "airpress_stash_".microtime(true);
+			set_transient($stash_key,$this->deferredQueries,60);
+			//trigger async request to process stashed queries
+			$start = microtime(true);
+			airpress_debug(0,__FUNCTION__."|"."Sending ASYNC request to process ".count($this->deferredQueries)." deferred queries.");
+			wp_remote_post(
+				admin_url( 'admin-ajax.php' )."?action=airpress_deferred&stash_key=".urlencode($stash_key),
+				array(
+						"blocking"  => false,
+						"timeout"   => 0.01 ,
+						'sslverify' => apply_filters( 'https_local_ssl_verify', false )
+					)
+			);
+			airpress_debug(0,__FUNCTION__."|"."DONE in ".(microtime(true) - $start)." seconds");
+		}
+	}
+
+	function defer_query($query){
+		$hash = $query->hash();
+		$this->deferredQueries[$hash] = clone $query;
+	}
+
+	function run_deferred_queries($stash_key){
+		$deferred_queries = get_transient($stash_key);
+		delete_transient($stash_key);
+		airpress_debug(0,__FUNCTION__."|"."Processing ".count($deferred_queries)." queries");
+		foreach($deferred_queries as $hash => $query){
+			$results = AirpressConnect::_get($query);
+			airpress_debug(0,__FUNCTION__."|"."Query ".$query->toString()." had ".count($results)." records returned. Que Bella!");
+		}
+
+	}
+
+	function debug(){
+
+		$args = func_get_args();
+
+		$message = array_shift($args);
+		if (!is_string($message)){
+			ob_start();
+			var_dump($message);
+			$message = ob_get_clean();
+		}
+
+		if (!empty($args)){
+			$expanded = "";
+			foreach($args as $input){
+				if (!is_string($input)){
+					ob_start();
+					var_dump($input);
+					$expanded .= ob_get_clean()."<Br><Br>";
+				}
+			}
+
+			$this->debug_output .= "<a class='expander' href='#'>$message</a>";
+			$this->debug_output .= "<div class='expandable'>$expanded</div>";
+		} else {
+			$this->debug_output .= $message;
+		}
+
+		$this->debug_output .= "<br><br>";
+	
+	}
+
+	public function renderDebugOutput(){
+		?>
+		<style type="text/css">
+			#airpress_debugger {
+				width:100%;
+				font-family: monospace;
+				padding:50px;
+				font-size: 12px;
+				padding-top:100px;
+				display:none;
+				position: absolute;
+				top: 0px;
+				left: 0px;
+				background-color: #f7f7f7;
+				z-index:99998;
+			}
+			#airpress_debugger .expandable {
+				padding: 20px;
+				border: 1px dashed #cccccc;
+				display: none;
+				white-space: pre-wrap;
+			}
+		</style>
+		<script type="text/javascript">
+		jQuery(document).ready(function(){
+			jQuery("#wp-admin-bar-airpress_debugger_toggle").click(function(e){
+				jQuery("#airpress_debugger").toggle();
+			});
+
+			jQuery(".expander").click(function(e){
+				e.preventDefault();
+				jQuery(this).next().fadeToggle();
+			});
+		});
+		</script>
+		<div id="airpress_debugger">
+		<?php
+		foreach($this->debug_stats as $name => $value){
+			echo $name.": ".$value."<br>";
+		}
+		?>
+		<hr>
+		<?php echo $this->debug_output; ?>
+		</div>
+		<?php
+	}
+
+	function renderDebugToggle( $wp_admin_bar ) {
+		$args = array(
+			'id'    => 'airpress_debugger_toggle',
+			'title' => 'Toggle Airpress Debugger',
+			'href'  => '#',
+			'meta'  => array( 'class' => 'my-toolbar-page' )
+		);
+		$wp_admin_bar->add_node( $args );
+	}
+
+
+}
+?>
---
+++ trunk/airpress.php
@@ -0,0 +1,447 @@
+<?php
+/*
+Plugin Name: Airpress
+Plugin URI: http://chetmac.com/airpress
+Description: Extend Wordpress Posts, Pages, and Custom Fields with data from remote Airtable records.
+Version: 1.1.63
+Author: Chester McLaughlin
+Author URI: http://chetmac.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+$airpress_version = "1.1.63";
+
+if ( ! defined( 'WPINC' ) ) {
+	 die;
+}
+
+
+require_once("lib/chetmac/Airpress.php");
+require_once("lib/chetmac/AirpressConnect.php");
+require_once("lib/chetmac/AirpressQuery.php");
+require_once("lib/chetmac/AirpressRecord.php");
+require_once("lib/chetmac/AirpressCollection.php");
+require_once("lib/chetmac/AirpressVirtualFields.php");
+require_once("lib/chetmac/AirpressVirtualPosts.php");
+
+if (is_admin()){
+	require_once("lib/chetmac/AirpressAdmin.php");	
+	require_once("lib/chetmac/AirpressVirtualPostsAdmin.php");
+	require_once("lib/chetmac/AirpressVirtualFieldsAdmin.php");
+
+	if ( is_plugin_active("elementor/elementor.php") ){
+		// require_once("lib/chetmac/AirpressElementorWidget.php");
+
+		add_action( 'elementor/widgets/widgets_registered', 'register_airpress_elementor' );
+
+		function register_airpress_elementor(){
+				// Include Widget files
+			require_once( 'lib/chetmac/AirpressElementorWidget.php' );
+
+			// Register widget
+			\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \AirpressElementorWidget() );
+		}
+	}
+}
+
+$airpress = new Airpress();
+add_action( 'plugins_loaded', array($airpress,'init') );
+
+if ( is_admin()){
+	add_action( 'plugins_loaded', array($airpress,"check_for_update") );
+}
+
+if ( ! class_exists("Parsedown")){
+	require_once("lib/erusev/Parsedown.php");
+}
+
+if (!isset($parsedown)){
+	$parsedown = new Parsedown();
+	$parsedown->setBreaksEnabled(true);
+}
+
+// add ajax handle which will act as trigger
+add_action( 'wp_ajax_nopriv_airpress_deferred', 'airpress_execute_deferred_queries' );
+add_action( 'wp_ajax_airpress_deferred', 'airpress_execute_deferred_queries' );
+
+register_activation_hook( __FILE__, 'airpress_activation' );
+register_deactivation_hook( __FILE__, 'airpress_deactivation' );
+
+if ( ! function_exists("airpress_activation") ){
+
+	function airpress_activation(){
+
+		flush_rewrite_rules();
+
+	}
+
+}
+
+if ( ! function_exists("airpress_deactivation") ){
+
+	function airpress_deactivation(){
+
+		flush_rewrite_rules();
+
+	}
+
+}
+
+
+function airpress_get_current_record(){
+	global $airpress, $post;
+
+	if ( is_airpress_collection($post->AirpressCollection) ){
+
+		if ( ! empty($airpress->loopscope) ){
+			return $airpress->loopscope[0];
+		} else {
+			return $post->AirpressCollection[0];
+		}
+
+	}
+
+}
+
+function airpress_execute_deferred_queries() {
+	global $airpress;
+
+	$airpress->run_deferred_queries($_GET["stash_key"]);
+
+	wp_send_json_success(array());
+}
+
+
+function is_airpress_attachment($input=null){
+	return is_array($input) && isset($input[0]["url"]);	
+}
+
+function is_airpress_collection($input=null){
+	if (isset($input) && is_object($input) && get_class($input) == "AirpressCollection"){
+		return true;
+	}
+
+	return false;
+}
+
+function is_airpress_empty($input=null){
+	if (!is_airpress_collection($input) || !isset($input[0]) || !is_airpress_record($input[0])){
+		return true;
+	}
+
+	return false;
+}
+
+function is_airpress_record($input=null){
+	if (isset($input) && is_object($input) && get_class($input) == "AirpressRecord"){
+		return true;
+	}
+
+	return false;
+}
+
+function get_airpress_configs($option_group,$run_filter=true){
+	$configs = array();
+	$id = 0;
+	while ($config = get_airpress_config($option_group,$id) ){
+
+		$config["id"] = $id; // never assumed the ID will be the same
+		$configs[] = $config;
+		$id++;
+	}
+
+	if (count($configs) == 1 && $configs[0]["name"] == "New Configuration"){
+		$configs = array();
+	}
+
+	if ($run_filter){
+		$configs = apply_filters( 'airpress_configs', $configs, $option_group );
+	}
+
+	return $configs;
+}
+
+function get_airpress_config($option_group, $id){
+	if ( !is_int($id) ){
+		$configs = get_airpress_configs($option_group);
+		foreach($configs as $config){
+			if ($id == $config["name"]){
+				break;
+			}
+		}
+	} else {
+		$config = get_option($option_group.$id);
+	}
+
+	if (!$config)
+		return false;
+
+	return $config;
+}
+
+function delete_airpress_config($option_group, $id){
+	$options = get_airpress_configs($option_group,false);
+
+	foreach($options as $key => $option){
+		delete_option($option_group.$key);
+	}
+
+	unset($options[$id]);
+	$options = array_values($options);
+
+	foreach($options as $key => $option){
+		$option["id"] = $key;
+		add_option($option_group.$key,$option);
+	}
+
+}
+
+function set_airpress_config($option_group, $id, $config){
+	update_option($option_group.$id,$config);
+}
+
+function is_airpress_force_fresh($config=0){
+	
+	if ( ! is_array($config) ){
+		$config = get_airpress_config("airpress_cx",$config);
+	}
+
+	$fresh_param = $config["fresh"];
+
+	if (isset($_GET[$fresh_param]) && $_GET[$fresh_param] == "true"){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function airpress_getArrayValue($array,$keys){
+	// for backwards compatibility
+	return airpress_getArrayValues($array,$keys);
+}
+
+function airpress_getArrayValues($array,$keys){
+	while(!empty($keys)){
+		$key = array_shift($keys);
+
+		if (isset($array[$key])){
+			$array = $array[$key];
+		} else {
+			// Maybe it's an array of arrays
+			$return_array = array();
+			foreach($array as $item){
+				if ( isset($item[$key]) ){
+					$return_array[] = $item[$key];
+				}
+			}
+			if ( ! empty($return_array) ){
+				$array = $return_array;
+			}
+		}
+	}
+	return $array;
+}
+
+function airpress_parse_template($template, $record, $replacementFields=null, $trim=true){
+
+	if ( is_null($replacementFields) ){
+	    preg_match_all("/{{([^}]*)}}/", $template, $matches);
+
+	    $replacementFields = array_unique($matches[1]);
+	}
+
+	foreach($replacementFields as $replacementField){
+
+		$keys = explode("|", $replacementField);
+		
+		if ( ! empty($keys) && $trim ){
+			$keys = array_map(function($field){
+				return trim($field);
+			}, $keys);
+		}
+
+		$field = array_shift($keys);
+		$replacementValue = "";
+		
+		if ( strtolower($field) == "record_id" ){
+			if ( is_airpress_record($record)){
+				$replacementValue = $record->record_id();
+			} else {
+				airpress_debug(0,"Attempting to populate field $field on a non-populated record",$keys);
+			}
+		} else if ( isset($record[$field]) ){
+
+			if ( ! is_airpress_empty( $record[$field] ) ){ 
+				// this means it IS an AirpressCollection with records
+
+				if (empty($keys)){
+					// this shouldn't really happen because we can't output a collection
+					// we should be looking INSIDE the collection, but can't since keys is empty
+				} else {
+					$replacementValue = implode(", ", $record[$field]->getFieldValues($keys) );
+				}
+
+			// This field is an array
+			} else if (is_array($record[$field]) ){
+
+				if (empty($keys)){
+					$replacementValue = implode(", ",$record[$field]);
+				} else {
+					$array = $record[$field];
+					while (!empty($keys)){
+						$key = array_shift($keys);
+						$array = $array[$key];
+					}
+					if (is_array($array)){
+						$replacementValue = implode(", ",$array);
+					} else {
+						$replacementValue = $array;
+					}
+				}
+
+			} else if (isset($record[$field])){
+
+				$replacementValue = $record[$field];
+
+			}
+
+		}
+
+		$template = str_replace("{{".$replacementField."}}", $replacementValue, $template);
+
+	}
+
+	return $template;
+
+}
+
+function airpress_flush_cache($all=false){
+
+	global $wpdb;
+
+	$to_delete = array();
+	$expirations = $wpdb->get_results( 'SELECT * FROM wp_options WHERE option_name LIKE "_transient_timeout_aprq_%" ORDER BY option_value ASC', OBJECT );
+
+	$exp = array();
+	foreach($expirations as $row){
+		$hash = str_replace("_transient_timeout_aprq_","",$row->option_name);
+		if (time() >= $row->option_value || $all === true ){
+			$to_delete[] = "_transient_timeout_aprq_".$hash;
+			$to_delete[] = "_transient_aprq_".$hash;
+		}
+	}
+
+	if (!empty($to_delete)){
+		$in = '"'.implode('","', $to_delete).'"';
+		$wpdb->get_results( 'DELETE FROM wp_options WHERE option_name IN ('.$in.')', OBJECT );
+	}
+
+}
+
+function airpress_debug($cx=0,$message=null,$object=null){
+	global $airpress;
+
+	if ( !isset($airpress)){
+		// somehow this happens with wp cli
+		return;		
+	}
+
+	if ( is_null($message) ){
+		$message = $cx;
+		$cx = 0;
+	}
+
+	if ( ! is_array($cx) ){
+		$config = get_airpress_config("airpress_cx",$cx);
+	} else {
+		$config = $cx;
+	}
+	
+	$object_string = "";
+
+	if ( isset($config["debug"]) ){
+
+		if ( $config["debug"] == 1 || $config["debug"] == 2 ){
+
+			$object_string = print_r($object,true);
+			$object_string = str_replace(substr($config["api_key"],3,-3),"*******",$object_string);
+			$object_string = str_replace(substr($config["app_id"],3,-3),"*******",$object_string);
+
+			if ( $h = @fopen($config["log"], "a") ){
+				fwrite($h, $message."\n");
+				if (isset($object)){
+					fwrite($h, "#########################################\n".$object_string."\n#########################################\n\n");
+				}
+				fclose($h);
+			}
+
+		}
+
+		if ( $config["debug"] == 1 || $config["debug"] == 3 ){
+
+			if ( ! is_null($object) ){
+				// ob_start();
+				// var_dump($object);
+				// $expanded = ob_get_clean();;
+
+				$airpress->debug_output .= "+ <a class='expander' href='#'>$message</a>";
+				$airpress->debug_output .= "<div class='expandable'>".print_r($object_string,true)."</div>";
+				//$airpress->debug_output .= "<div class='expandable'>$expanded</div>";
+			} else {
+				$airpress->debug_output .= $message;
+			}
+
+			$airpress->debug_output .= "<br><br>";
+
+		}
+
+	}
+	
+}
+
+// Look at me being backwards compatible while 
+// working towards using namespaced functions.
+if ( ! function_exists("is_cornerstone") ){
+	function is_cornerstone(){
+		return is_airpress_cornerstone();
+	}
+}
+
+function is_airpress_cornerstone(){
+	if (isset($_GET["action"]) && $_GET["action"] == "cs_render_element"){
+		// Cornerstone Element Render
+		return "render";
+	} else if (isset($_GET["cornerstone_preview"])){
+		// Cornerstone Admin Preview iFrame
+		return "preview";
+	} else if (isset($_GET["cornerstone"])){
+		// Cornerstone Admin
+		return "admin";
+	}
+
+	return false;
+}
+
+function is_airpress_elementor(){
+	if (isset($_POST["action"]) && $_POST["action"] == "elementor_ajax"){
+		// elementor Element Render
+		return "render";
+	}
+
+	return false;
+}
+
+function is_airpress_compatible_page_builder(){
+
+	if ( $mode = is_airpress_cornerstone() ){
+		return $mode;
+	}
+
+	if ( $mode = is_airpress_elementor() ){
+		return $mode;
+	}
+	
+	return false;
+
+}
